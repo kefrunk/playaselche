b9229230095052ae35fc9c4841c13e9871e035e4
Eduardo <edwargan@gmail.com>
2013-05-16 13:19:30 +0200

    ss

---
+++ messages/en/guia.php
@@ -0,0 +1,25 @@
+<?php 
+return array(
+	'elchetitulo' => 'Elche y Comarca',
+	'vinalopotitulo' => 'Vinalopo Medio y Alto',
+	
+	'intro playas Alicante' => 'La ciudad de Alicante est&aacute; ba&ntilde;ada por las aguas del mediterr&aacute;neo, y magnificas playas se extienden por toda su costa. Desde playas m&iacute;ticas como la playa de San Juan o la del Postiguet, pasando por las de la Almadrava, La Albufereta, urbanova o las de muchavista o Arenales pertenecientes a munipios lindantes. Sin olvidar la bella isla de Tabarca a la que se puede acceder por barco desde el puerto de la ciudad.',
+	'intro playas Elche' => 'Entre Arenales del Sol y La Marina se pueden encontrar algunas de las playas m&aacute;s bellas y espaciosas de la provincia de Alicante.',
+	'intro playas Elche 2' => 'Destacan algunas como la playa del Carabass&iacute;, desde donde se ve de fondo la ciudad de Alicante al estar ubicada en medio de la bah&iacute;a, o las de Santa Pola, siendo las m&aacute;s tur&iacute;sticas por sus completas instalaciones. Las calas recuerdan a las de Tabarca que se encuentra a s&oacute;lo 25 minutos desde el puerto de Santa Pola. Al sur est&aacute;n las de La Marina, caracterizadas por sus dunas, y la del pinet que posee una pinada donde podr&aacute; disfrutar de una playa con car&aacute;cter 100% natural.',
+	'intro arenales' => 'Cuenta con una playa de arena fina dorada en una zona de alta belleza, que presenta adem&aacute;s un sistema dunar &uacute;nico',
+	'intro arenales 2' => 'Su extensi&oacute;n es de 2.800 metros por 40 metros de ancho. El nivel medio de ocupaci&oacute;n es alto. Playa protegida urbana sin paseo mar&iacute;timo. El estado habitual de la mar es oleaje moderado . Playa con acceso a minusv&aacute;lidos, parking, aseos, duchas, tel&eacute;fono, alquiler de sombrillas, alquiler de hamacas y tumbonas, deportes n&aacute;uticos.',
+	'intro Gran Playa' => 'Cuenta on una zona n&aacute;utica de windsurf y patines, un chiringuito, restaurantes y una rampa habilitada para el acceso a embarcaciones. Sus finas arenas y su gran anchura permiten realizar gran variedad de actividades, juegos deportivos, actividades l&uacute;dicas y n&aacute;uticas. Esta playa est&aacute; dotada de un punto accesible para aquellas personas con movilidad reducida, con todos los equipamientos e infraestructuras necesarios para el disfrute de estos usuarios.',
+	'intro Playa Levante' => '<p>Situada en la zona m&aacute;s c&eacute;ntrica del municipio y con una amplia gama de servicios e infraestructuras, adem&aacute;s cuenta con una gran oferta de juegos infantiles y deportivos para hacer el reposo lo m&aacute;s agradable posible. Esta playa est&aacute; dotada de un punto accesible para aquellas personas con movilidad reducida, con todos los equipamientos e infraestructuras necesarios para el disfrute de estos usuarios.Playa galardonada con el distintivo Bandera Azul 2011.',
+	'intro Varadero' => 'Dispone de todos los servicios de las playas urbanas y de zona n&aacute;utica de patines. Playa galardonada con el distintivo Bandera Azul 2011.',
+	'intro Calas Santa Pola' =>'Son calas de arena intercaladas con zonas rocosas y con limpias aguas de un intenso color azul. Algunas de ellas declaradas de &quot;Inter&eacute;s Tur&iacute;stico Nacional&quot;',
+	'intro Tamarit' => 'Dispone de todos los servicios de las playas urbanas. Playa galardonada con el distintivo Bandera Azul 2011.',
+	'intro La Marina' => '<p>Es una playa de arena fina dorada, abierta y virgen que ofrece unas limpias y azules aguas. Su entorno est&aacute; marcado por una importante franja de dunas que le dan una atractiva imagen de playa natural.',
+	
+	
+	'subtitulo elche Fiestas' => 'Fiestas',
+	'subtitulo elche Hoteles' => 'Hoteles',
+	'subtitulo elche Restaurantes' => 'Restaurantes',
+	'subtitulo elche Telofonos' => 'Tekefonos de Interes',
+	'Mas Info' => 'Mas Info',
+);
+?>
